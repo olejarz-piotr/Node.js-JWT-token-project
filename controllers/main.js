b3d1f1567a68993ef10826a9ad3727f6b56e9844
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-10-30 20:34:29 +0200

    adding custom errors

--- controllers/main.js
+++ controllers/main.js
@@ -9,23 +9,19 @@ const login = async (req, res) => {
     }
 
     const id = new Date().getDate();
-    const token = jwt.sign({id, username},process.env.JWT_SECRET, {expiresIn:'30d'})
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
 
 
-    res.status(200).json({msg:'user created', token})
+    res.status(200).json({ msg: 'user created', token })
 }
 
 
 const dashboard = async (req, res) => {
 
-    const authHeader= req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new CustomAPIError('Please provide email and password', 400)
-
-
-    }
+   console.log(req.user);
     const luckynr = Math.floor(Math.random() * 100)
-    res.status(200).json({ msg: `Hello bro`, secret: `Here is your number ${luckynr}` })
+        res.status(200).json({ msg: `Hello bro`, secret: `Here is your number ${luckynr}` })
+
 
 
 }
